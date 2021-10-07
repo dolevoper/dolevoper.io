d17c20c9e4fa44f0e56c662a18920d2742655b27
Omer Dolev <odolev@microsoft.com>
2021-10-07 20:40:13 +0300

    style blog page and add another blog entry

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,17 +1,27 @@
 import * as React from "react";
 import { graphql, Link } from "gatsby";
+import { GatsbyImage, getImage } from "gatsby-plugin-image";
 import Layout from "../../components/layout";
+import { post, coverImage, publishDate } from "./index.module.css";
 
 const BlogPage = ({ data }) => {
     return (
         <Layout title="Blog">
             <h2>Blog</h2>
-            {data.allMdx.nodes.map(({ id, slug, frontmatter }) => (
-                <article key={id}>
-                    <h3><Link to={`/blog/${slug}`}>{frontmatter.title}</Link></h3>
-                    <p>Published: {frontmatter.date}</p>
-                </article>
-            ))}
+            {data.allMdx.nodes.map(({ id, slug, frontmatter }) => {
+                const image = getImage(frontmatter.hero_image);
+
+                return (
+                    <article key={id} className={post}>
+                        <figure>
+                            <GatsbyImage image={image} alt={frontmatter.hero_image_alt} className={coverImage}  />
+                        </figure>
+                        <h3><Link to={`/blog/${slug}`}>{frontmatter.title}</Link></h3>
+                        <p className={publishDate}>Published: {frontmatter.date}</p>
+                        <p>{frontmatter.description}</p>
+                    </article>
+                );
+            })}
         </Layout>
     );
 };
@@ -23,6 +33,13 @@ export const query = graphql`
                 frontmatter {
                     date(formatString: "MMMM D, YYYY")
                     title
+                    description
+                    hero_image_alt
+                    hero_image {
+                        childImageSharp {
+                            gatsbyImageData
+                        }
+                    }
                 }
                 id
                 slug
