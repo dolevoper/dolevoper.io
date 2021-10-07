6446adbb8d1adf54a948e4453289b5d67f55f50d
Omer Dolev <odolev@microsoft.com>
2021-10-07 12:34:01 +0300

    add missing parts to blog post

--- src/pages/blog/{mdx.slug}.js
+++ src/pages/blog/{mdx.slug}.js
@@ -1,13 +1,29 @@
 import * as React from "react";
 import { graphql } from "gatsby";
-import Layout from "../../components/layout";
 import { MDXRenderer } from "gatsby-plugin-mdx";
+import { GatsbyImage, getImage } from "gatsby-plugin-image";
+import Layout from "../../components/layout";
+import ExternalLink from "../../components/externalLink";
 
 const BlogPostPage = ({ data }) => {
     const { frontmatter, body } = data.mdx;
+    const image = getImage(frontmatter.hero_image);
+    
     return (
         <Layout title={frontmatter.title}>
             <h2>{frontmatter.title}</h2>
+            <p>{frontmatter.description}</p>
+            <p>{frontmatter.date}</p>
+            {
+            image &&
+                <figure>
+                    <GatsbyImage image={image} alt={frontmatter.hero_image_alt} />
+                    <figcaption>
+                        Photo by{" "}
+                        <ExternalLink href={frontmatter.hero_image_credit_link}>{frontmatter.hero_image_credit_text}</ExternalLink>
+                    </figcaption>
+                </figure>
+            }
             <MDXRenderer>{body}</MDXRenderer>
         </Layout>
     );
@@ -17,8 +33,17 @@ export const query = graphql`
     query($slug: String) {
         mdx(slug: {eq: $slug}) {
             frontmatter {
-                title
                 date(formatString: "MMMM D, YYYY")
+                title
+                description
+                hero_image_alt
+                hero_image_credit_link
+                hero_image_credit_text
+                hero_image {
+                    childImageSharp {
+                        gatsbyImageData
+                    }
+                }
             }
             body
         }
