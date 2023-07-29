b7fdfd13a489f964cbdc0480ecaba580e0483a1f
Omer Dolev <omerdolev90@gmail.com>
2023-07-29 23:48:53 +0300

    style post footer

--- app/blog/[slug]/PostFooter.tsx
+++ app/blog/[slug]/PostFooter.tsx
@@ -1,6 +1,8 @@
 import Link from "next/link";
 import { getSiblings } from "../posts";
 
+import styles from "./PostFooter.module.css";
+
 type PostFooterProps = {
     slug: string
 };
@@ -8,16 +10,16 @@ export default async function PostFooter({ slug }: PostFooterProps) {
     const { previous, next } = await getSiblings(slug);
 
     return (
-        <footer>
+        <footer className={styles.footer}>
             <nav>
-                <ul>
-                    {previous && <li>
-                        <Link href={previous.slug}>{previous.title}</Link>
-                    </li>}
-                    <li><Link href="#">Back to top</Link></li>
+                <ul className={styles.navLinks}>
                     {next && <li>
-                        <Link href={next.slug}>{next.title}</Link>
+                        <Link href={next.slug}>Next: {next.title}</Link>
                     </li>}
+                    {previous && <li>
+                        <Link href={previous.slug}>Back to: {previous.title}</Link>
+                    </li>}
+                    <li className={styles.backToTop}><Link href="#">Back to top</Link></li>
                 </ul>
             </nav>
         </footer>
