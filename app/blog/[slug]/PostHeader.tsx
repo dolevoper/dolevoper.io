61c3e6c5bc3651596c7bdcf84217cdf250a79176
Omer Dolev <omerdolev90@gmail.com>
2023-07-29 23:52:55 +0300

    refactor styles

--- app/blog/[slug]/PostHeader.tsx
+++ app/blog/[slug]/PostHeader.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
-import styles from "./page.module.css";
 import { getPost } from "../posts";
+
+import styles from "./PostHeader.module.css";
 
 type PostHeaderProps = {
     slug: string
