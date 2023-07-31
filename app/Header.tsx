f37692046c43aa7a6eee78c768ac484fc8e18b0f
Omer Dolev <omerdolev90@gmail.com>
2023-08-01 01:13:01 +0300

    wip

--- app/Header.tsx
+++ app/Header.tsx
@@ -2,6 +2,7 @@
 import { usePathname } from "next/navigation";
 import Link from "next/link";
 import styles from "./Header.module.css";
+import { useReducer, useState } from "react";
 
 const links = [
     { text: "About", href: "/" },
@@ -10,11 +11,22 @@ const links = [
 ];
 
 export default function Header() {
-    const pathname = usePathname()
+    const pathname = usePathname();
+    const [isNavMenuOpen, toggleIsNavMenuOpen] = useReducer((value) => !value, false);
 
     return (
         <header className={styles.header}>
-            <nav>
+            {isNavMenuOpen && <div className={styles.backdrop} onClick={toggleIsNavMenuOpen}></div>}
+            <label htmlFor="isNavMenuOpen" role="button" aria-controls="primary-navigation" aria-expanded={isNavMenuOpen}>
+                <svg viewBox="0 0 100 100" width="1em" stroke="currentColor" fill="currentColor">
+                    <rect width={80} height={10} x={10} y={25} rx={5} />
+                    <rect width={80} height={10} x={10} y={45} rx={5} />
+                    <rect width={80} height={10} x={10} y={65} rx={5} />
+                </svg>
+                <span className={styles.visuallyHidden}>Open menu</span>
+            </label>
+            <input id="isNavMenuOpen" type="checkbox" checked={isNavMenuOpen} onClick={toggleIsNavMenuOpen} />
+            <nav id="primary-navigation">
                 <ul className={styles.siteLinks}>
                     {links.map(({ text, href }, index) => (
                         <li key={index}>
